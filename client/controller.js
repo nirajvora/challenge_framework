cd9826609e8cfc2ec23d04888688d29b81917d98
Niraj Vora <nsvora@ucdavis.edu>
2015-12-14 21:44:30 -0800

    organizes user feedback and color codes completed requirements

--- client/controller.js
+++ client/controller.js
@@ -1,7 +1,38 @@
-app.controller('PageController', ['$scope', function ( $scope ) { // This serves as the controller for the one-page web app
-  $scope.$on('update', function ( event, data ) {
-    $scope.$apply(function (){ $scope.structure = data; });
+app.controller('PageController', ['$scope', 'CodeFactory', function ( $scope, CodeFactory ) { // This serves as the controller for the one-page web app
+  $scope.$on('update', function ( event, data, log) {
+    $scope.$apply(function ( ) {
+      
+      $scope.structure = data;
+      $scope.error = 'No Errors';
+
+      if(CodeFactory.isInside( data, 'ForStatement', 'IfStatement')) {
+        $scope.blackStyle = {color: 'red'};
+      } else {
+        $scope.blackStyle = {color: 'green'};
+      }
+      if (CodeFactory.doesExist( log, 'VariableDeclaration' ) && CodeFactory.doesExist( log, 'ForStatement')) {
+        $scope.whiteStyle = {color: 'green'};
+      } else {
+        $scope.whiteStyle = {color: 'red'};
+      }
+
+    });
   });
 
   $scope.structure = "";
+
+  window.onerror = function ( ) {
+    $scope.$apply(function ( ) {
+      $scope.error = 'ERROR: look at your console!';
+    });
+  };
+
+  $scope.error = 'No Errors';
+
+  $scope.whiteStyle = {color: 'red'};
+  $scope.blackStyle = {color: 'green'};
+
+  $scope.task = 'Create an array consisting of only numbers. Add all numbers in the array larger than 3';
+  $scope.whiteList = 'There must be at least one variable declaration';
+  $scope.blackList = 'There must not be an if statement inside the for loop';
 }]);
--- client/factory.js
+++ client/factory.js
@@ -3,22 +3,57 @@ app.factory('CodeFactory', [ '$rootScope', function ( $rootScope ) { // This fac
   var parse = function( string ) {
     var object = acorn.parse(string);
     var array = [];
-    (function walk( node ) {
-      array.push(node.type);
+    var typeLog = {};
+    (function walk( node, prevNode ) {
+      var expression;
+      if(prevNode === null || prevNode === 'Program') {
+        expression = node.type;
+      } else {
+        expression = node.type + ' inside ' + prevNode;
+      }
+      
+      array.push(expression);
+      typeLog[node.type] = true;
+
       if (node.body === undefined) {return;}
       if (node.body.length === undefined) {
-        walk(node.body);
+        walk(node.body, expression);
       } else {
         for(var i = 0; i < node.body.length; i++) {
-          walk(node.body[i]);
+          walk(node.body[i], expression);
         }
       }
-    })(object);
+    })(object, null);
+    array.shift();
 
-    $rootScope.$broadcast('update', array);
+    $rootScope.$broadcast('update', array, typeLog);
+  };
+
+  var isInside = function (array, outside, inside) {
+    var wrapped = false;
+    array.forEach(function ( node ) {
+      var check = node.split(' inside ');
+      var isWrapped = false;
+      check.forEach(function ( innderNode ) {
+        if(innderNode === inside) { isWrapped = true; }
+        if(innderNode === outside && isWrapped === true) { wrapped = true; }
+      });
+    });
+    return wrapped;
+  };
+
+  var doesExist = function(object, target) {
+    if (target in object) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return {
-    parse: parse
+    parse: parse,
+    isInside: isInside,
+    doesExist: doesExist
   };
+
 }]);
